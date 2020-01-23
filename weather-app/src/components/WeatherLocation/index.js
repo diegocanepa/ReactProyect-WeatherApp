41260bc016233e43dd0c4bc7d7542926fa75f211
diegocanepa <diego.canepa241198@gmail.com>
2020-01-23 14:56:13 -0300

    Video 50 por la mitad

--- weather-app/src/components/WeatherLocation/index.js
+++ weather-app/src/components/WeatherLocation/index.js
@@ -12,18 +12,20 @@ import {
     WINDY,
 } from '../../constants/weathers.js';
 
+const location = "Buenos Aires, ar";
+const api_key = "bc652012ca8d9f42b17205d98def2d6e";
+const url_base_weather = "https://api.openweathermap.org/data/2.5/weather";
+/*
+UNA FORMA DE TRANSFORMAR LOS DATOS DE KELVIN A CELSIUS, 
+PERO VAMOS A USAR UNA LIBRERIA NOSOTROS
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}&units=metric`;
+*/
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
 const data = {
     temperature: 5,
     weatherState: SUN,
     humidity: 10,
     wind: '10 m/s',
-};
-
-const data2 = {
-    temperature: 25,
-    weatherState: WINDY,
-    humidity: 50,
-    wind: '30 m/s',
 };
 
 class WeatherLocation extends Component {
@@ -36,12 +38,25 @@ class WeatherLocation extends Component {
         };
     };
 
+       
     //FUNCION QUE SE EJECUTA CUANDO SE TOCA EL BOTON CON EL EVENTO ONCLICK
     handleUpdateClick = () =>{
-        console.log("Actualizdo");
-        this.setState({
-            city:"Cordoba",
-            data: data2,
+        /*
+        lo que hace el fetch es que te permite el retorno 
+        de los datos de la appi. SI o SI se le tiene que mandar el
+        URL
+        */
+        fetch(api_weather).then((resolve) => {
+            console.log(resolve);
+            
+            return resolve.json();
+        }).then((data) => {
+            const newWeather = this.getData(data);
+            console.log(newWeather)
+            debugger;
+            this.setState({
+                data: newWeather,
+            });
         });
     };
 
