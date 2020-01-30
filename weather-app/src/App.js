2fbd79dfd50f1bf35e84f6c3fd2d2eaae455a797
diegocanepa <diego.canepa241198@gmail.com>
2020-01-30 14:55:23 -0300

    VIDEO 91

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -18,11 +18,21 @@ const cities = [
 ];
 
 class App extends Component {
+  constructor() {
+    super()
+    //la sentencia set.state se usa solo en el constructor
+    this.state = {city: null}
+  }
+  
   handleSelectedLocation = (city) => {
     console.log(`handleSelectedLocation ${city}`);
+    this.setState({
+      city: city
+    })
   };
 
   render() {
+    const {city} = this.state;
   return (
     <Grid>
       <Row>
@@ -44,9 +54,11 @@ class App extends Component {
         <Col xs={12} md={6}>
           <Paper elevation={4}>
             <div className="details">
-              <ForecastExtended city={"Cordoba"}>
-
-              </ForecastExtended>
+              {
+              !city ?
+                null :
+                <ForecastExtended city={city}></ForecastExtended>
+              }
             </div>
           </Paper> 
         </Col>
--- weather-app/src/components/ForecastExtended.js
+++ weather-app/src/components/ForecastExtended.js
@@ -1,12 +1,39 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import './styles.css';
+import ForecastItem from './ForecastItem';
 
+
+const days = [
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+];
+
+const data = {
+    temperature: 10,
+    humidity: 50,
+    weatherState: 'normal',
+    wind: '10 m/s',
+};
 
 class ForecastExtended extends Component {
+    renderForecastItemsDays(days){
+        return (
+        days.map( day => (<ForecastItem weekDay = {day} hour={10} data={data}></ForecastItem>))
+        )
+    }
+
     render() {
         const {city} = this.props;
         return (
-            <div>PronosticoExtendido para ${city}</div>
+            <div>
+                <h2 className='forecast-title'>Pronostico Extendido para {city}</h2>
+                {this.renderForecastItemsDays(days)}
+
+            </div>
         )
     }
 }
